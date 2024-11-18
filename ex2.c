3ae6561584a1261d89e7b63e7618dc0dd362edac
Ludimila Guerra <167739772+LudimilaGuerra@users.noreply.github.com>
2024-11-18 20:33:41 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
 int main()
 {
 	float tl, as, ef, mp;
@@ -9,28 +12,19 @@ int main()
 	scanf("%f", &as);
 
 	printf("Por fim, adicione a nota do exame final:\n");
-	scanf("%d", &ef);
+	scanf("%f", &ef);
 
-	mp = ((tl*2) + (as*3) + (ef*5))/(2+3+5);
+	mp = (tl*2 + as*3 + ef*5)/10;
 	
-	if(mp==8.00 && mp<=10.00)
-	{
-		printf("A");
-	}
-	if(mp<8.00 && mp>=7.00)
-	{
-		printf("B");
-	}
-	if(mp<7.00 && mp>=6.00)
-	{
-		printf("C");
-	}
-	if(mp<6.00 && mp>=5.00)
-	{
-		printf("D");
-	}
-	else
-	{
-		printf("E");
-	}
+	if (mp >= 8.0 && mp <= 10.0) {
+        printf("a");
+    } else if (mp >= 7.0 && mp < 8.0) {
+        printf("b");
+    } else if (mp >= 6.0 && mp < 7.0) {
+        printf("c");
+    } else if (mp >= 5.0 && mp < 6.0) {
+        printf("d");
+    } else {
+        printf("e");
+    }
 }
